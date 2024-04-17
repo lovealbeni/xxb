b22239f633639de844e45833639f5b43102f3745
zhengshibin <shibin.zheng@1521335688226052.onaliyun.com>
2024-04-17 18:16:21 +0800

    feat: add package react-window for virtual list

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [splitStr, setSplitStr] = useState("");
-  const [name, setName] = useState("");
+  const [value,setValue] = useState<string>("我们中出了一个叛徒")
 
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke("greet", { name }));
-  // }
+  const showValue = split(value);
 
-  async function split(input:string) {
+  async function split(input:string):Promise<string[]> {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     // setSplitStr(await invoke("split", { input }));
     const res = await invoke("split", { input });
-    setSplitStr(res.join(", "));
+    return res as Promise<string[]>;
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          split(name);
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{splitStr}</p>
+      <input onChange={(e) => setValue(e.target.value)} value={value}></input>
+      <div>retust</div>
+      {
+        showValue.map((item,index)=>{
+          return <div key={index}>{item}</div>
+        })
+      }
     </div>
   );
 }
