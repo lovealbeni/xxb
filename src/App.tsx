c2ae2f7ff0c8ba76d9ec346a4e5d25f6c05bd1fc
zhengshibin <shibin.zheng@1521335688226052.onaliyun.com>
2024-04-18 10:41:57 +0800

    add debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { debounce } from "lodash";
 
 function App() {
-  const [value,setValue] = useState<string>("我们中出了一个叛徒")
+  const [splitValue, setSplitValue] = useState<string[]>([]);
+  const [_, setInputValue] = useState<string>("");
 
-  const showValue = split(value);
+  const split = async (value: string) => {
+    const res = await invoke("split", { input: value });
+    setSplitValue(res as string[]);
+  };
 
-  async function split(input:string):Promise<string[]> {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // setSplitStr(await invoke("split", { input }));
-    const res = await invoke("split", { input });
-    return res as Promise<string[]>;
-  }
+  const splitDebounce = useCallback(debounce(split, 500), []);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+    splitDebounce(e.currentTarget.value);
+  };
 
   return (
     <div className="container">
-      <input onChange={(e) => setValue(e.target.value)} value={value}></input>
+      <input onChange={onChangeHandler}></input>
       <div>retust</div>
-      {
-        showValue.map((item,index)=>{
-          return <div key={index}>{item}</div>
-        })
-      }
+      {splitValue.map((item, index) => {
+        return <div key={index}>{item}</div>;
+      })}
     </div>
   );
 }
