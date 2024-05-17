89d18d74dab99ccbdbe8ca477e3d17e5677ed1e0
zhengshibin <shibin.zheng@1521335688226052.onaliyun.com>
2024-05-17 14:43:44 +0800

    feat: 修改样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { debounce } from "lodash";
 
 function App() {
+  const editorRef = useRef<HTMLDivElement>(null);
   const [splitValue, setSplitValue] = useState<string[]>([]);
-  const [_, setInputValue] = useState<string>("");
 
-  const split = async (value: string) => {
+  const split = async () => {
+    const value = editorRef.current?.innerText;
+    // todo 这里split /n 就行
     const res = await invoke("split", { input: value });
     setSplitValue(res as string[]);
   };
 
+  const editHtml = splitValue.map((value, index) => {
+    return `<span key=${index}>${value}</span>`
+  }).join("");
+
   const splitDebounce = useCallback(debounce(split, 500), []);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.currentTarget.value);
-    splitDebounce(e.currentTarget.value);
+    splitDebounce();
   };
 
   return (
-    <div className="container">
-      <input onChange={onChangeHandler}></input>
-      <div>retust</div>
-      {splitValue.map((item, index) => {
-        return <div key={index}>{item}</div>;
-      })}
+    <div contentEditable suppressContentEditableWarning ref={editorRef} onInput={onChangeHandler} dangerouslySetInnerHTML={{__html: editHtml}}>
     </div>
   );
 }
