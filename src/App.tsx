555f05363ac15a187175520dff2ae0908a96534e
zhengshibin <shibin.zheng@1521335688226052.onaliyun.com>
2024-05-20 10:30:22 +0800

    :x :x

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useRef } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import { debounce } from "lodash";
 
 function App() {
   const editorRef = useRef<HTMLDivElement>(null);
-  const [splitValue, setSplitValue] = useState<string[]>([]);
 
   const split = async () => {
+    debugger
     const value = editorRef.current?.innerText;
-    // todo 这里split /n 就行
     const res = await invoke("split", { input: value });
-    setSplitValue(res as string[]);
   };
-
-  const editHtml = splitValue.map((value, index) => {
-    return `<span key=${index}>${value}</span>`
-  }).join("");
 
   const splitDebounce = useCallback(debounce(split, 500), []);
 
@@ -25,7 +19,7 @@ function App() {
   };
 
   return (
-    <div contentEditable suppressContentEditableWarning ref={editorRef} onInput={onChangeHandler} dangerouslySetInnerHTML={{__html: editHtml}}>
+    <div contentEditable suppressContentEditableWarning ref={editorRef} onInput={onChangeHandler}>
     </div>
   );
 }
